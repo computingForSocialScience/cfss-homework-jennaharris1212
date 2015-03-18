3dc9dc134dfc82c2def5bf6a902e64e48417228d
jennaharris1212 <jennaharris1212@users.noreply.github.com>
2015-03-18 15:32:13 -0500

    Update create_tables.py

--- final_project/create_tables.py
+++ final_project/create_tables.py
@@ -8,10 +8,10 @@ import matplotlib.pyplot as plt
 # created database in mysql (before it stopped working) called "facebook"
 # within this we created two tables "friend_attributes" and "friend_edges"
 # error: "ERROR 1045 (28000): Access denied for user 'root'@'localhost' (using password: NO)""
-dbname="facebook"
+dbname="friends"
 host="127.0.0.1"
 user="root"
-passwd="newpwd"
+passwd=" "
 db=pymysql.connect(db=dbname, host=host, user=user,passwd=passwd, charset='utf8')
 
 cur = db.cursor()
@@ -24,8 +24,8 @@ cur = db.cursor()
 def getFriendAttributes(attributes):
 	attribute_data = []
 	with open('friend_attributes') as f:
-    	for line in f:
-        	data.append(json.loads(line))#loads data from json
+		for line in f:
+			data.append(json.loads(line))#loads data from json
 	return attribute_data #return this LIST of IDs
 	# takes attribute_data from json and iterates it
 	for a in attribute_data:
@@ -40,22 +40,23 @@ def getFriendAttributes(attributes):
 		birthday_date = a['birthday_date']
 		new_list.append(birthday_date)
 
-		pic = a['pic']
-		new_list.append(pic)
-
+		sex = a['sex']
+		new_list.append(sex)
+	#list is created with all of the retrieved data
 	return new_list
 
-fill_attributes_table= """INSERT INTO friend_attributes (uid, name, birthday_date, pic) VALUES (%s, %s, %s, %s);"""
+#take the newly created list and insert it into the attributes table
+fill_attributes_table= """INSERT INTO friend_attributes (uid, name, birthday_date, sex) VALUES (%s, %s, %s, %s);"""
 cur.execute(fill_attributes_table, new_list) # fill the table with appended list
 
 def getFriendEdges(attributes):
 	edge_data = []
 	with open('friend_edges') as e:
-    	for line in e:
-        	data.append(json.loads(line))
-    return edge_data
+		for line in e:
+			data.append(json.loads(line))
+		return edge_data
 
-    for b in edge_data:
+	for b in edge_data:
 		edge_list = []
 
 		uid = a['uid1']
@@ -70,8 +71,10 @@ fill_edge_table= """INSERT INTO friend_edges (uid1, uid2) VALUES (%s, %s);"""
 cur.execute(fill_edge_table, edge_list) # fill the table with appended list
 
 
-# function that opens the list of friend attributes
-# and appends the selected attributes from the json file to the new list
 
 db.commit()
 cur.close()
+
+# at this point MySQL is unaccessible so we cannot check if our code runs
+# we would normally check it before moving forward, but we just moved on 
+# assuming the code works
