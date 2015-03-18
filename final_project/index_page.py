7f0a0474f79ae7b5ff01075a1ccc44664f3203b6
jennaharris1212 <jennaharris1212@users.noreply.github.com>
2015-03-18 16:40:50 -0500

    Update index_page.py

--- final_project/index_page.py
+++ final_project/index_page.py
@@ -18,27 +18,31 @@ cur = db.cursor()
 app=Flask(__name__)
 
 @app.route('/') #base web address
+# this function just renders index.html when
+# someone goes to http://127.0.0.1:5000/
 def make_index_resp():
-    # this function just renders index.html when
-    # someone goes to http://127.0.0.1:5000/
-    db=pymysql.connect(db=dbname, host=host, user=user,passwd=passwd, charset='utf8')
-	c = db.cursor()
-	c.execute('''SELECT uid, name FROM friend_attributes ORDER BY name;''')
-	friend_attributes = c.fetchall()
-	#this will return a list of Friend on the webpage
+    # we are taking the uid and names from the file friend_attributes
+    # since we imported pymysql earlier, mysql commands should function
+	cur.execute('''SELECT uid, name FROM friend_attributes ORDER BY name;''')
+	friend_attributes = cur.fetchall()
+	#this will return a list of Friends on the index page
     return(render_template('index.html', friend_attributes=friend_attributes)) 
 
 @app.route('/profile/<uid>') #static route
 def make_profiles_resp():
 	# goes to database, finds friend and their selected 
 	# attributes on their profile from the MySQL Table
-	cur.execute('''SELECT name, birthday_date, sex 
-	FROM friends
-	WHERE uid=%s;''')
-	friend_attributes = cur.fetchall()
+	cur.execute('''SELECT * FROM friend_attributes''')
+	# created a dictionary to put the tuple 
+	d = {}
+	for tup in cur.fetchall():
+		print tup
+		(uid, name, birthday_date, sex) = tup
+		d[uid] = name
 	#this displays the attributes on a friend's profile page
 	return render_template('friends.html',uid=uid)
-
+	
 
 if __name__ == '__main__': 
 	app.run() 
+
